248c4e2db21daf3e6761e0c08bb7e0a3035e9c16
Rob <rob_452@hotmail.co.uk>
2026-07-09 10:12:09 +0100

    Initial commit with basic home page

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="main">
+      <h1>Emotional Intelligence</h1>
+      <p>Welcome. This is a static Next.js + TypeScript site.</p>
+    </main>
+  );
+}
